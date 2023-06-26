6fc664fd2d63338201d8948a736e0b91cfa28634
Hossein <Rahmatei@gmail.com>
2023-06-26 22:08:10 +0330

    Add List And Create And Delete Roles

---
+++ AAASamples/Controllers/RoleController.cs
@@ -0,0 +1,64 @@
+﻿using AAASamples.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AAASamples.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult List()
+        {
+            var roles= _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityRole = new IdentityRole(model.Name);
+                var result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("list");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string Name)
+        {
+            var identityRole=await _roleManager.FindByNameAsync(Name);
+            if (identityRole != null)
+            {
+                var result = await _roleManager.DeleteAsync(identityRole);
+                //if (!result.Succeeded)
+                //{
+                //    foreach (var item in result.Errors)
+                //    {
+                //        ModelState.AddModelError("", item.Description);
+                //    }
+                //}
+            }
+            return RedirectToAction("list");
+        }
+    }
+}
---
+++ AAASamples/Models/RoleModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AAASamples.Models
+{
+    public class RoleModel
+    {
+    }
+    public class CreateRoleViewModel
+    {
+        public string Name { get; set; }
+
+    }
+}
